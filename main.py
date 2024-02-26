024726cf33c4eeba49eec827300989405369a518
cvilmorin <carre@aims.ac.za>
2024-02-26 14:45:03 +0200

    perceptron

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ import numpy as np
 
 class Perceptron():
     def __init__(self,feat_nb,obs_nb):
-        self.weight=np.random.rand((1,feat_nb))
-        self.biais=np.random.rand((1,1))
+        self.weight=np.random.rand(1,feat_nb)
+        self.biais=np.random.rand(1)
+
     def forward(self,x):
         return self.weight*x +self.biais
 
+    def softmax(self,Z):
+        A = np.exp(Z) / sum(np.exp(Z))
+        return A
+    #def train(self):
+
+
 perceptron=Perceptron(1,4)
-print(perceptron.biais)
+x=np.random.rand(4,3)
+x=x.T
+s=perceptron.softmax(perceptron.forward(x))
+print(s)
+
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+print(get_predictions(s))
---
+++ perceptron.py
@@ -0,0 +1,66 @@
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+import numpy as np 
+from tqdm import tqdm
+class Perceptron:
+    def __init__(self,X,y,epoch,lr):
+        self.weight=np.random.randn(X.shape[1],1)
+        self.biais=np.random.randn(1)
+        self.lr=lr
+        self.epoch=epoch
+    
+    def sigmoid(self,Z):
+        return 1/(np.exp(-Z)+1)
+    
+    def model(self,X):
+        Z=X.dot(self.weight)+self.biais
+        return self.sigmoid(Z)
+    
+    def log_loss(self,A,y):
+        return -1/len(y)*np.sum(y*np.log(A)+(1-y)*np.log(1-A))
+    
+    def gradients(self,A,X,Y):
+        dW=1/len(y)*np.dot(X.T,A-y)
+        db=1/len(y)*np.sum(A-y)
+        return (dW,db)
+    
+    def update(self,dW,db):
+        weight=self.weight-self.lr*dW
+        biais=self.biais-self.lr*db
+        return weight,biais
+
+    def predict(self,X):
+        A = self.model(X)
+        return A >= 0.5
+
+
+    def fit(self,X,y):
+        loss=[]
+        for i in tqdm(range(self.epoch)):
+            A=self.model(X)
+            loss.append(self.log_loss(A,y))
+            dW,db=self.gradients(A,X,y)
+            self.weight,self.biais=self.update(dW,db)
+        y_pred=self.predict(X)
+        print(accuracy_score(y,y_pred))
+        plt.plot(loss)
+        plt.show()
+        
+
+
+X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+y = y.reshape((y.shape[0], 1))
+
+
+for i in tqdm(range(5)):
+    print(i)
+
+
+
+#print('dimensions de X:', X.shape)
+#print('dimensions de y:', y.shape)
+p=Perceptron(X,y,epoch=10000,lr=1e-1)
+p.fit(X,y)
+print(X[1],y[1])
+print(p.predict(X[1]))
